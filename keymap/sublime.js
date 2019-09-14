730e75d5f2bdab5b4d6ea5f3ede360af5eaf9af3
Stuart <stuart@s16n.com>
2019-09-13 22:18:11 -0700

    Update sublime.js

--- keymap/sublime.js
+++ keymap/sublime.js
@@ -6,7 +6,7 @@
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../lib/codemirror"), require("../addon/search/searchcursor"), require("../addon/edit/matchbrackets"));
+    mod(require("../addon/search/searchcursor"), require("../addon/edit/matchbrackets"));
   else if (typeof define == "function" && define.amd) // AMD
     define(["../lib/codemirror", "../addon/search/searchcursor", "../addon/edit/matchbrackets"], mod);
   else // Plain browser env
